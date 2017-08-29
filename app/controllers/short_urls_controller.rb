3e2273d1715142d81bb17d663e38c92a48386a8a
mdfarhanjawed <mdfarhanjawed@yahoo.com>
2017-08-30 01:07:54 +0530

    Added Styling

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -8,14 +8,16 @@ class ShortUrlsController < ApplicationController
 	end
 
 	def create		
-		url        = ShortUrl.get_host_without_www(params[:search]).gsub(/.com/,'') if params[:search]
-		url_id     = ShortUrl.bijective_decode(url)	
+		if params[:search].present?			
+			url        = ShortUrl.get_host_without_www(params[:search]).gsub(/.com/,'') 
+			url_id     = ShortUrl.bijective_decode(url)	
 
-		if record = ShortUrl.find_by(short_url_id: url_id)				
-			record.count = record.count + 1
-			record.save
-		else
-			ShortUrl.create(url: params[:search], short_url_id: url_id, count: 1)
+			if record = ShortUrl.find_by(short_url_id: url_id)				
+				record.count = record.count + 1
+				record.save
+			else
+				ShortUrl.create(url: params[:search], short_url_id: url_id, count: 1)
+			end
 		end
 
 		redirect_to short_urls_path	
